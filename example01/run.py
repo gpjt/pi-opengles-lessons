35d97635955612377e65de7eac15bee73d1d6a78
Giles Thomas <giles@giles.net>
2017-06-23 21:39:28 +0000

    Shrank mandelbrot shader to a level where I can try to work out why it's broken.

--- example01/run.py
+++ example01/run.py
@@ -12,56 +12,21 @@ FRAGMENT_SHADER = """
         float cx = vPosition.x;
         float cy = vPosition.y;
 
-        float hue;
-        float saturation;
-        float value;
-        float hueRound;
-        int hueIndex;
-        float f;
-        float p;
-        float q;
-        float t;
-
-
         float x = 0.0;
         float y = 0.0;
         float tempX = 0.0;
-        int i = 0;
         int runaway = 0;
         for (int i=0; i < 100; i++) {
-            tempX = x * x - y * y + float(cx);
-            y = 2.0 * x * y + float(cy);
-            x = tempX;
-            if (runaway == 0 && x * x + y * y > 100.0) {
-                runaway = i;
-            }
+             tempX = x * x - y * y + float(cx);
+             y = 2.0 * x * y + float(cy);
+             x = tempX;
+             if (runaway == 0 && x * x + y * y > 100.0) {
+                 runaway = 1;
+             }
         }
 
         if (runaway != 0) {
-            hue = float(runaway) / 200.0;
-            saturation = 0.6;
-            value = 1.0;
-
-            hueRound = hue * 6.0;
-            hueIndex = int(mod(float(int(hueRound)), 6.0));
-            f = fract(hueRound);
-            p = value * (1.0 - saturation);
-            q = value * (1.0 - f * saturation);
-            t = value * (1.0 - (1.0 - f) * saturation);
-
-            if (hueIndex == 0)
-                gl_FragColor = vec4(value, t, p, 1.0);
-            else if (hueIndex == 1)
-                gl_FragColor = vec4(q, value, p, 1.0);
-            else if (hueIndex == 2)
-                gl_FragColor = vec4(p, value, t, 1.0);
-            else if (hueIndex == 3)
-                gl_FragColor = vec4(p, q, value, 1.0);
-            else if (hueIndex == 4)
-                gl_FragColor = vec4(t, p, value, 1.0);
-            else if (hueIndex == 5)
-                gl_FragColor = vec4(value, p, q, 1.0);
-
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
         } else {
             gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
         }
@@ -178,6 +143,7 @@ def draw_scene(egl, gl, shader_program, shape):
     for x, y in BASE_CORNERS:
         corners.append(x / shape.zoom + shape.center_offset_x)
         corners.append(y / shape.zoom + shape.center_offset_y)
+    print(f"corners: {corners}")
     gl.bufferData(gl.ARRAY_BUFFER, corners, gl.STATIC_DRAW);
     gl.vertexAttribPointer(
         shader_program.plot_position_attribute,

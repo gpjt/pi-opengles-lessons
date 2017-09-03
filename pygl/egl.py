a019539e1b0aa796123ffde97aa299a40431be6d
Giles Thomas <giles@giles.net>
2017-09-03 21:33:16 +0100

    Better error-handling

--- pygl/egl.py
+++ pygl/egl.py
@@ -4,6 +4,7 @@ import ctypes
 from pygl import egl_constants
 
 # Define some extra constants that the automatic extraction misses
+egl_constants.EGL_FALSE = 0
 egl_constants.EGL_DEFAULT_DISPLAY = 0
 egl_constants.EGL_NO_CONTEXT = 0
 egl_constants.EGL_NO_DISPLAY = 0
@@ -28,17 +29,16 @@ def eglints(L):
 class EGL(object):
 
     def __init__(self):
-        """Opens up the OpenGL library and prepares a window for display"""
         b = bcm.bcm_host_init()
         if b != 0:
             raise Exception("Could not initialize Pi GPU")
 
         self.display = openegl.eglGetDisplay(egl_constants.EGL_DEFAULT_DISPLAY)
-        if self.display == 0:
-            raise Exception("Could not open EGL display")
+        if self.display == egl_constants.EGL_NO_DISPLAY:
+            raise Exception("Could not open EGL display: {}".format(openegl.eglGetError()))
 
-        if openegl.eglInitialize(self.display, 0, 0) == 0:
-            raise Exception("Could not initialise EGL")
+        if openegl.eglInitialize(self.display, 0, 0) == egl_constants.EGL_FALSE:
+            raise Exception("Could not initialise EGL: {}".format(openegl.eglGetError()))
 
         attribute_list = eglints((
             egl_constants.EGL_RED_SIZE, 8,
@@ -49,7 +49,7 @@ class EGL(object):
             egl_constants.EGL_SURFACE_TYPE, egl_constants.EGL_WINDOW_BIT,
             egl_constants.EGL_NONE
         ))
-                                                                    
+
         numconfig = eglint()
         config = ctypes.c_void_p()
         r = openegl.eglChooseConfig(
@@ -59,11 +59,11 @@ class EGL(object):
             ctypes.byref(numconfig)
         )
         if r == 0:
-            raise Exception("Could not choose EGL config")
+            raise Exception("Could not choose EGL config: {}".format(openegl.eglGetError()))
 
         r = openegl.eglBindAPI(egl_constants.EGL_OPENGL_ES_API)
         if r == 0:
-            raise Exception("Could not bind config")
+            raise Exception("Could not bind config: {}".format(openegl.eglGetError()))
 
         context_attribs = eglints((egl_constants.EGL_CONTEXT_CLIENT_VERSION, 2, egl_constants.EGL_NONE))
         self.context = openegl.eglCreateContext(
@@ -72,7 +72,7 @@ class EGL(object):
             ctypes.byref(context_attribs)
         )
         if self.context == egl_constants.EGL_NO_CONTEXT:
-            raise Exception("Could not create EGL context")
+            raise Exception("Could not create EGL context: {}".format(openegl.eglGetError()))
 
         width = eglint()
         height = eglint()
@@ -108,9 +108,9 @@ class EGL(object):
         if self.surface == egl_constants.EGL_NO_SURFACE:
             raise Exception("Could not create surface")
 
-        r = openegl.eglMakeCurrent(self.display, self.surface, self.surface, self.context) 
+        r = openegl.eglMakeCurrent(self.display, self.surface, self.surface, self.context)
         if r == 0:
-            raise Exception("Could not make our surface current")
+            raise Exception("Could not make our surface current: {}".format(openegl.eglGetError()))
 
 
     def get_context(self):
